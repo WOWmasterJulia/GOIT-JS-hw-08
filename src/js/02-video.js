2c3ca4bf1409737f40887a243b8d8d67c41f5cea
WOWmasterJulia <topazzz@ukr.net>
2023-04-05 01:12:19 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,21 @@
+import { throttle } from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+console.log(iframe);
+const player = new Vimeo.Player(iframe);
+console.log(player);
 
-// const iframe = document.querySelector('#vimeo-player');
-// console.log(iframe);
-// const player = new Vimeo.Player(iframe);
-// console.log(player);
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", String(data.seconds))
+}, 100));
+
+// import Player from '@vimeo/player';
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
 // player.on('play', function() {
-//         console.log('played the video!');
+//     console.log('played the video!');
 // });
-// player.getVideoTitle().then(function(title) {
-// console.log('title:', title);
-// });
-import Player from '@vimeo/player';
-
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
-
-player.on('play', function() {
-    console.log('played the video!');
-});
