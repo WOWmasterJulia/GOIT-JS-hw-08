0a2fa0e3a4fabd3c2e3bb30156b03572805bb43b
WOWmasterJulia <96340846+WOWmasterJulia@users.noreply.github.com>
2023-04-04 21:14:30 +0000

    Deploying to gh-pages from @ WOWmasterJulia/GOIT-JS-hw-08@4a47d2d061253857e9f870bd688dd760381714ad 🚀

--- 02-video.428d4f2c.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector("#vimeo-player");console.log(o);var e=new Vimeo.Player(o);console.log(e),e.on("play",(function(){console.log("played the video!")})),e.getVideoTitle().then((function(o){console.log("title:",o)}))}();
-//# sourceMappingURL=02-video.428d4f2c.js.map
--- 02-video.442f0582.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("#vimeo-player");console.log(o);const e=new Vimeo.Player(o);console.log(e),e.on("play",(function(){console.log("played the video!")})),e.getVideoTitle().then((function(o){console.log("title:",o)}));
-//# sourceMappingURL=02-video.442f0582.js.map
