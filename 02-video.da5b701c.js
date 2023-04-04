f9864c63e93cb4ac2f3ba0b22db7dee22b22a30e
WOWmasterJulia <96340846+WOWmasterJulia@users.noreply.github.com>
2023-04-04 20:39:50 +0000

    Deploying to gh-pages from @ WOWmasterJulia/GOIT-JS-hw-08@53be0748cdf554350668524f05e09b1767e8717c 🚀

---
+++ 02-video.da5b701c.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("#vimeo-player");console.log(o);const e=new Vimeo.Player(o);console.log(e),e.on("play",(function(){console.log("played the video!")}));
+//# sourceMappingURL=02-video.da5b701c.js.map
