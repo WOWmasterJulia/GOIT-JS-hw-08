53be0748cdf554350668524f05e09b1767e8717c
WOWmasterJulia <topazzz@ukr.net>
2023-04-04 23:39:20 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,3 +6,22 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+
+const container = document.querySelector('.gallery');
+const markup = galleryItems.map((elem) => `<li class="gallery__item">
+  <a class="gallery__link" href="${elem.original}">
+    <img
+      class="gallery__image"
+      src="${elem.preview}"
+      alt="${elem.description}"/>
+  </a>
+</li>`);
+container.insertAdjacentHTML('beforeend', markup.join(''));
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    
+});
+console.log(gallery);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
 
+const iframe = document.querySelector('#vimeo-player');
+console.log(iframe);
+const player = new Vimeo.Player(iframe);
+console.log(player);
+player.on('play', function() {
+        console.log('played the video!');
+    });
