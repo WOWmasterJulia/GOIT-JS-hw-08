dda5e1c8607213b05faf5156b3797e3670a22f1d
WOWmasterJulia <topazzz@ukr.net>
2023-04-06 23:36:30 +0300

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -26,18 +26,19 @@ const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
 };
+console.log(refs.form);
 refs.form.addEventListener("submit", onFormSubmit);
 // refs.textarea.addEventListener("input", onTextareaInput);
-refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
+// refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
 // 7.Для того, чтобы поле майл тоже выводилось:
-refs.form.addEventListener('input', e => {
-    // console.log(e.target.name);
+refs.form.addEventListener('input', throttle(e => {
+    // console.log('FFFFFFFFFFFF', e.target.name);
     // console.log(e.target.value);
     formData[e.target.name] = e.target.value;
     console.log(formData);
-
+    localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
     // Сделать так, чтобы сохраняло не только сообщение, но и имя, и все в одном обьекте?????
-});
+},500));
 
 populateTextarea();
 
@@ -59,17 +60,18 @@ function onFormSubmit(evt) {
 function onTextareaInput(evt) {
     // const message = evt.currentTarget.value;
     const message = evt.target.value;
-    const mail = 
+    console.log('процедура ontext')
     localStorage.setItem('STORAGE_KEY', message);
 }
 
 // 3.Получаем значение из хранилища
 // Если там что-то было, обновляем DOM
 function populateTextarea() {
-    const savedMessage = localStorage.getItem('STORAGE_KEY');
+    const savedMessage = JSON.parse(localStorage.getItem('STORAGE_KEY'));
     if (savedMessage) {
         console.log(savedMessage);
-        refs.textarea.value = savedMessage;
+        refs.textarea.value = savedMessage.message;
+        refs.form[0].value = savedMessage.email;
     }
     
 }
