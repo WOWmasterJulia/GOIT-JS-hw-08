6db601334ec92488ec78ae8522ef4ac1da24e241
WOWmasterJulia <topazzz@ukr.net>
2023-04-06 12:14:29 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,11 +12,3 @@ player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
 player.on('timeupdate', throttle((data) => {
     localStorage.setItem("videoplayer-current-time", String(data.seconds))
 }, 1000));
-
-// import Player from '@vimeo/player';
-// const iframe = document.querySelector('#vimeo-player');
-// const player = new Player(iframe);
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
